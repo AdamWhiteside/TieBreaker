ad7fbc690dfde4b3289c94bb359f702f53fa8df9
stormdavis6 <stormdavis6@yahoo.com>
2020-01-28 16:16:23 -0500

    added btn on main activity to launch coin flip game

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSettleTheScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSettleTheScore = (Button) findViewById(R.id.btnSettleTheScore);
+        btnSettleTheScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCoinFlipActivity();
+            }
+        });
     }
+    public void openCoinFlipActivity() {
+        Intent intent = new Intent(this,CoinFlipActivity.class);
+        startActivity(intent);
+    }
+
 }
