4dba4dadac30caf50d0fe51a64ad927c6e802f8d
Wlamm <LammW16@students.ecu.edu>
2020-02-12 10:14:06 -0500

    TicTacToe Game first attempt

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,12 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSettleTheScore = (Button) findViewById(R.id.btnSettleTheScore);
+        btnSettleTheScore = findViewById(R.id.btnSettleTheScore);
         btnSettleTheScore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openPopUpActivity();
+                openTicTacToeActivity();
+                //openPopUpActivity();
             }
         });
     }
@@ -32,4 +28,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openTicTacToeActivity() {
+        Intent intent = new Intent(this, TicTacToeActivity.class);
+        startActivity(intent);
+    }
 }
+
