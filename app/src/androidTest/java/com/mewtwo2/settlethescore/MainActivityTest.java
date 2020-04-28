18a6054a380eb150254cbdf85e23d23cf993a16d
stormdavis6 <stormdavis6@yahoo.com>
2020-04-27 23:22:05 -0400

    completed main activity automated tests

--- app/src/androidTest/java/com/mewtwo2/settlethescore/MainActivityTest.java
+++ app/src/androidTest/java/com/mewtwo2/settlethescore/MainActivityTest.java
@@ -4,12 +4,19 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.mewtwo2.settlethescore.activities.CoinFlipActivity;
+import com.mewtwo2.settlethescore.activities.GameActivity;
 import com.mewtwo2.settlethescore.activities.MainActivity;
+import com.mewtwo2.settlethescore.activities.PickGameActivity;
+import com.mewtwo2.settlethescore.activities.PopUpActivity;
 import com.mewtwo2.settlethescore.activities.R;
+import com.mewtwo2.settlethescore.activities.RouletteActivity;
+import com.mewtwo2.settlethescore.activities.TicTacToeActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,26 +30,19 @@ import static junit.framework.TestCase.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-        assertEquals("com.mewtwo2.settlethescore", appContext.getPackageName());
-    }
-
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
             MainActivity.class);
 
     @Test
     public void SettleTheScoreBtnWorks() {
         onView(withId(R.id.btnSettleTheScore)).perform(click());
-        intended(hasComponent(CoinFlipActivity.class.getName()));
+        intended(hasComponent(PopUpActivity.class.getName()));
     }
 
+    @Test
     public void PickGameBtnWorks() {
-        onView(withId(R.id.btnSettleTheScore)).perform(click());
-        intended(hasComponent(CoinFlipActivity.class.getName()));
+        onView(withId(R.id.btnPickGame)).perform(click());
+        intended(hasComponent(PickGameActivity.class.getName()));
     }
 }
