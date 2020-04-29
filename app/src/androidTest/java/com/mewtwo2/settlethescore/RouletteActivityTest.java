f3ef153561bdf37c99255132c9ac9904404337b7
stormdavis6 <stormdavis6@yahoo.com>
2020-04-28 23:21:13 -0400

    automated testing scripts

--- app/src/androidTest/java/com/mewtwo2/settlethescore/RouletteActivityTest.java
+++ app/src/androidTest/java/com/mewtwo2/settlethescore/RouletteActivityTest.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.mewtwo2.settlethescore.activities.CoinFlipActivity;
+import com.mewtwo2.settlethescore.activities.PopUpActivity;
 import com.mewtwo2.settlethescore.activities.R;
 import com.mewtwo2.settlethescore.activities.RouletteActivity;
 
@@ -18,7 +21,14 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static junit.framework.TestCase.assertEquals;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class RouletteActivityTest {
@@ -29,7 +39,9 @@ public class RouletteActivityTest {
             RouletteActivity.class);
 
     @Test
-    public void RouletteBtnWorks() {
+    public void TextViewDisplaysValueAfterSpin() {
         onView(withId(R.id.spinBtn)).perform(click()); //spin button works
+        // passes if the textView does not match the empty string
+        onView(withId(R.id.resultTv)).check(matches(not(withText(""))));
     }
 }
